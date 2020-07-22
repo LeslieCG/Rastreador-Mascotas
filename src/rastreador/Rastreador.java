8d7228b8c928bb734e29185265eaba6132f4c9e6
Leslie\Leslie cardenas <leslie.cardgtz002@gmail.com>
2020-07-21 20:30:17 -0500

    Proyecto rastreador de Lenguajes de Interfaz

---
+++ src/rastreador/Rastreador.java
@@ -0,0 +1,13 @@
+
+package rastreador;
+
+
+public class Rastreador {
+
+    public static void main(String[] args) {
+        inicio uno = new inicio();
+        uno.setVisible(true);
+       
+    }
+    
+}
